d176c5b4874c9254e7b528f4dcbc1ff4d9ad74bd
Fedepedersen <99141783+Fedepedersen@users.noreply.github.com>
2022-10-26 13:24:24 -0300

    Agregué los praácticos de JS

---
+++ bootcamp/practico2.2Condicionales/ejercicio9/ejercicio9.js
@@ -0,0 +1,5 @@
+//9) Escribí un código que pida dos nombres por pantalla, los almacene en una variable y que muestre una alerta que diga: “El primer valor ingresado es (valor1) y el segundo es (valor2)”.
+
+let pedido=prompt("Ingrese un nombre");
+let pedido2=prompt("Ingrese otro nombre");
+alert(`Los nombres ingresados son: ${pedido} y ${pedido2}`);
---
+++ bootcamp/practicoLoops/ejercicio4.js
@@ -0,0 +1,7 @@
+//4)
+//Momento de agregar arrays a la práctica.
+//Escribí un código en el cual crees un array con los números del 1 al 10. (Este array lo vamos a seguir usando en los próximos ejercicios) y luego mediante el uso de un bucle, imprimí cada número en consola.
+let miArray=[1,2,3,4,5,6,7,8,9,10];
+for (let index = 0; index< miArray.length; index++) {
+    console.log(miArray[index]);
+}
