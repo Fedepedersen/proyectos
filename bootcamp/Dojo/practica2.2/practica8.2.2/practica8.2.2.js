7eb41a97fcb42e924209d8128e5540ccd1b9dc0e
Federico Ariel Pedersen <fedepedersen123@gmail.com>
2022-10-31 19:59:06 -0300

    blabla

---
+++ bootcamp/Dojo/practica2.2/practica8.2.2/practica8.2.2.js
@@ -0,0 +1,5 @@
+/* 8)
+Escribí un código que pida haga lo mismo que el ejercicio anterior pero esta vez que muestre el valor como una alerta que diga : “El valor ingresado es (el valor)”. */
+
+let valor = prompt('Ingrese un valor');
+alert(`El valor ingresado es ${valor89}`);
